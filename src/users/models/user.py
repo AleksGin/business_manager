ff36180bfd107d331b5a0b20fb5aec97f319d2e0
AleksGin <alexanderginin@icloud.com>
2025-04-15 18:26:31 +0300

    created user model

---
+++ src/users/models/user.py
@@ -0,0 +1,61 @@
+from datetime import date
+from typing import List
+from uuid import UUID
+
+from sqlalchemy import (
+    Enum,
+    ForeignKey,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+from core.models import Base
+
+
+class RoleEnum(Enum):
+    Empl: str = "Employee"
+    Manager: str = "Manager"
+    Admin: str = "Administrator"
+
+
+class GenderEnum(Enum):
+    male: str = "Man"
+    female: str = "Woman"
+
+
+class Users(Base):
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(nullable=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_verified: Mapped[bool] = mapped_column(default=False)
+
+    name: Mapped[str] = mapped_column(nullable=False)
+    surname: Mapped[str] = mapped_column(nullable=False)
+    gender: Mapped["GenderEnum"] = mapped_column(nullable=False)
+    birth_date: Mapped[date] = mapped_column(nullable=False)
+    role: Mapped["RoleEnum"] = mapped_column(nullable=True)
+    departament_uuid: Mapped[UUID] = mapped_column(
+        f"{ForeignKey(Structure.__tablename__)}.uuid",
+        ondelete="SET NULL",
+        nullable=False,
+    )
+    departament: Mapped["Structure"] = relationship(
+        "Structure",
+        back_populates="workers",
+    )
+    head_of: Mapped["StructureElement"] = relationship(
+        "StructureElement",
+        back_populates="director",
+    )
+    messages: Mapped[List["Message"]] = relationship(
+        "Message",
+        back_populates="author"
+    )
+    tasks: Mapped[List["Tasks"]] = relationship(
+        "Task",
+        back_populates="performer"
+    )
+    
