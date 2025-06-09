b67ab5b43811e82d31c5feca8bba144cd15b8308
AleksGin <alexanderginin@icloud.com>
2025-06-09 18:48:51 +0300

    updated Team model: add new relationships and some changes

--- src/teams/models/team.py
+++ src/teams/models/team.py
@@ -1,6 +1,10 @@
-from typing import TYPE_CHECKING
+from typing import (
+    TYPE_CHECKING,
+    List,
+)
 from uuid import UUID
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
@@ -10,12 +14,27 @@ from sqlalchemy.orm import (
 from core.models import Base
 
 if TYPE_CHECKING:
-    from users.models import Users
+    from users.models import User
+    from tasks.models import Task
+
 
 class Team(Base):
-    name: Mapped[str] = mapped_column()
-    description: Mapped[str] = mapped_column()
-    owner_id: Mapped["Users"] = relationship(
-        "Users",
-        back_populates="team"
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    owner_uuid: Mapped[UUID] = mapped_column(
+        ForeignKey("users.uuid"),
+        nullable=False,
+    )
+    owner: Mapped["User"] = relationship(
+        "User",
+        back_populates="owned_teams",
+    )
+    members: Mapped[List["User"]] = relationship(
+        "User",
+        foreign_keys="User.team_uuid",
+        back_populates="team",
+    )
+    tasks: Mapped[List["Task"]] = relationship(
+        "Task",
+        back_populates="team",
     )
