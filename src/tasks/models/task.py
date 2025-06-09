4d54e627eeb37db996c95b78eb652a41e793f37f
AleksGin <alexanderginin@icloud.com>
2025-06-09 18:48:07 +0300

    created Task model and add relationships to User and Task model

---
+++ src/tasks/models/task.py
@@ -0,0 +1,64 @@
+import enum as PyEnum
+from datetime import datetime
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+from sqlalchemy import (
+    Enum,
+    ForeignKey,
+    String,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+from core.models import Base
+
+if TYPE_CHECKING:
+    from teams.models import Team
+    from users.models import User
+
+
+class StatusEnum(PyEnum.Enum):
+    OPENED = "Open"
+    IN_PROGRESS = "In progress"
+    DONE = "Done"
+
+
+class Task(Base):
+    title: Mapped[str] = mapped_column(String(80), nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    deadline: Mapped[datetime] = mapped_column(nullable=False)
+    status: Mapped[StatusEnum] = mapped_column(
+        Enum(StatusEnum),
+        nullable=False,
+        default=StatusEnum.OPENED,
+    )
+    assignee_uuid: Mapped[UUID] = mapped_column(
+        ForeignKey("users.uuid", ondelete="SET NULL"),
+        nullable=True,
+    )
+    team_uuid: Mapped[UUID] = mapped_column(
+        ForeignKey("teams.uuid", ondelete="CASCADE"),
+        nullable=False,
+    )
+    creator_uuid: Mapped[UUID] = mapped_column(
+        ForeignKey("users.uuid"),
+        nullable=False,
+    )
+    assignee: Mapped["User"] = relationship(
+        "User",
+        foreign_keys=[assignee_uuid],
+        back_populates="assigned_tasks",
+    )
+    creator: Mapped["User"] = relationship(
+        "User",
+        foreign_keys=[creator_uuid],
+        back_populates="created_tasks",
+    )
+    team: Mapped["Team"] = relationship(
+        "Team",
+        back_populates="tasks",
+    )
