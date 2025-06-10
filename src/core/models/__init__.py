f83fff835d6876bd3765790a84e0dc6499a89da9
AleksGin <alexanderginin@icloud.com>
2025-06-10 20:23:49 +0300

    сread association model for relation many-to-many

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "db_helper",
     "Base",
+    "meeting_participants",
 )
 
 from .db_helper import db_helper
 from .base import Base
+from .associations import meeting_participants
