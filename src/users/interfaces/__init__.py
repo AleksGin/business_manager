e77e4fed1750927561b7b6ae61abc470f87b12a6
AleksGin <alexanderginin@icloud.com>
2025-06-14 13:01:12 +0300

    add all interfaces to export

---
+++ src/users/interfaces/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "UserRepository",
+    "PasswordHasher",
+    "UserValidaor",
+    "UUIDGenerator",
+    "DBSession",
+)
+
+from .interfaces import (
+    DBSession,
+    PasswordHasher,
+    UserRepository,
+    UserValidaor,
+    UUIDGenerator,
+)
