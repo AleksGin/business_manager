f8548fe2a70eb2a33ea22d5f6711ab6f926dd319
AleksGin <alexanderginin@icloud.com>
2025-06-27 19:05:11 +0300

    added new depends into dependecies.py

--- src/core/dependencies/__init__.py
+++ src/core/dependencies/__init__.py
@@ -7,15 +7,29 @@ __all__ = (
     "UserActivationDep",
     "UUIDGeneratorDep",
     "CurrentUserDep",
+    # Teams
+    "TeamRepoDep",
+    "TeamMembershipDep",
+    # Tasks
+    "TaskRepoDep",
+    # Evaluations
+    "EvaluationRepoDep",
+    # Meetings
+    "MeetingRepoDep",
 )
 
 from .depends import (
     CurrentUserDep,
     PasswordHasherDep,
     SessionDep,
+    TeamMembershipDep,
+    TeamRepoDep,
     TokenRepoDep,
     UserActivationDep,
     UserRepoDep,
     UserValidatorDep,
     UUIDGeneratorDep,
+    TaskRepoDep,
+    EvaluationRepoDep,
+    MeetingRepoDep,
 )
